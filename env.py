97238a7369586bab0026c451e53961cc844524e1
GHOSTsama2503 <87672676+GHOSTsama2503@users.noreply.github.com>
2024-06-09 12:17:30 -0400

    first commit 2

---
+++ env.py
@@ -0,0 +1,8 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = os.environ["API_ID"]
+API_HASH = os.environ["API_HASH"]
+BOT_TOKEN = os.environ["BOT_TOKEN"]
---
+++ start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /home/ghost/alice
+source ./.venv/bin/activate
+python3 main.py
