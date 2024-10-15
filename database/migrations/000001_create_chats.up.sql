918f9dc03fb9e0d64260ba5479b3969ff99a42f2
GHOSTsama2503 <87672676+GHOSTsama2503@users.noreply.github.com>
2024-10-15 12:03:24 -0400

    fix: fixed typos in db migrations

--- database/migrations/000001_create_chats.up.sql
+++ database/migrations/000001_create_chats.up.sql
@@ -9,8 +9,8 @@ create table if not exists chats (
     id          bigint      primary key,
     type        ChatType    not null,
     title       text        not null,
-    created_at  timestmap   not null default current_timestamp,
-    updated_at  timestmap   not null default current_timestamp
+    created_at  timestamp   not null default current_timestamp,
+    updated_at  timestamp   not null default current_timestamp
 );
 
 create or replace trigger update_chats_updated_at
@@ -19,7 +19,7 @@ for each row
 execute function update_updated_at();
 
 create table if not exists chat_settings (
-    id                          bitint  primary key references chats(id) on update cascade on delete cascade,
+    id                          bigint  primary key references chats(id) on update cascade on delete cascade,
     is_captcha_enabled          boolean not null default false,
     is_welcome_message_enabled  boolean not null default false
 );
--- database/migrations/000002_create_users.up.sql
+++ database/migrations/000002_create_users.up.sql
@@ -1,8 +1,8 @@
 create table if not exists users (
-    id              bitint      primary key,
+    id              bigint      primary key,
     language_code   text,
-    created_at      timestmap   not null default current_timestamp,
-    updated_at      timestmap   not null default current_timestamp
+    created_at      timestamp   not null default current_timestamp,
+    updated_at      timestamp   not null default current_timestamp
 );
 
 create or replace trigger update_users_updated_at
@@ -13,7 +13,7 @@ execute function update_updated_at();
 create table if not exists user_stats (
     id          integer primary key generated always as identity,
     chat_id     bigint  not null references chats(id) on update cascade on delete cascade,
-    user_id     bitint  not null references users(id) on update cascade on delete cascade,
+    user_id     bigint  not null references users(id) on update cascade on delete cascade,
     warnings    integer not null default 0 check (warnings >= 0),
 
     unique (chat_id, user_id)
