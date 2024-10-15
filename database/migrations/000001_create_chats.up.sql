98ca87114a319c1c2db991ea59a478c55461a613
Xeraph <61700204+xeraph-dev@users.noreply.github.com>
2024-10-15 11:06:07 -0400

    refactor: split database migrations into separate files (#3)

---
+++ database/migrations/000001_create_chats.up.sql
@@ -0,0 +1,33 @@
+create or replace function update_updated_at() returns trigger as $$ begin
+    new.updated_at = current_timestamp;
+end; $$ language plpgsql;
+
+drop type if exists ChatType;
+create type ChatType as enum ('private', 'group', 'supergroup', 'channel');
+
+create table if not exists chats (
+    id          bigint      primary key,
+    type        ChatType    not null,
+    title       text        not null,
+    created_at  timestmap   not null default current_timestamp,
+    updated_at  timestmap   not null default current_timestamp
+);
+
+create or replace trigger update_chats_updated_at
+before update on chats
+for each row
+execute function update_updated_at();
+
+create table if not exists chat_settings (
+    id                          bitint  primary key references chats(id) on update cascade on delete cascade,
+    is_captcha_enabled          boolean not null default false,
+    is_welcome_message_enabled  boolean not null default false
+);
+
+create table if not exists chat_triggers (
+    chat_id bigint  primary key references chats(id) on update cascade on delete cascade,
+    key     text    not null,
+    message text    not null,
+
+    unique (chat_id, key)
+);
---
+++ database/migrations/000002_create_users.down.sql
@@ -0,0 +1,3 @@
+drop table if exists user_stats;
+drop trigger if exists update_users_updated_at on users;
+drop table if exists users;
